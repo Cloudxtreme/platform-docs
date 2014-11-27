b870f8b815f8b5d89dcb17f4feb8aed037d8ff66
Robbie Mackay <rm@robbiemackay.com>
2014-11-27 16:31:23 +1300

    Crawling all the things and saving them to disk

--- scripts/wiki-crawl-tree.py
+++ scripts/wiki-crawl-tree.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
 from Wiki import *
-import html2text
-from lxml import etree
-import os
-from pprint import pprint
+from common import *
+import os, re
 
 wiki = Wiki()
 
 h = html2text.HTML2Text()
 
+def cleanTitle(value):
+    value = re.sub(r'[^\w\s-]', r'-', value).strip().lower()
+    value = re.sub('[-\s]+', '-', value)
+    return value
+
+# Fetch children for a page and recurse
+def fetchChildren(page, prefix=''):
+    print prefix + '/' + cleanTitle(page['title'])
+
+    # Load the full page
+    fullpage = wiki.server.confluence2.getPage(wiki.token, page['id'])
+    # Convert it to markdown
+    markdown = convertToMarkdown(fullpage)
+    # Save the markdown
+    f = open(prefix + '/' + cleanTitle(page['title']) + '.md', 'w+')
+    f.write(markdown)
+    f.close()
+
+    # Append title to prefix and process children
+    prefix = prefix + '/' + cleanTitle(page['title'])
+    children = wiki.server.confluence2.getChildren(wiki.token, page['id'])
+    if (len(children) > 0):
+        if not os.path.isdir(prefix):
+            os.mkdir(prefix)
+        for child in children:
+            fetchChildren(child, prefix)
+
 if len(sys.argv) < 3:
-   exit("Usage: " + sys.argv[0] + " spacekey pagetitle")
+    exit("Usage: " + sys.argv[0] + " spacekey pagetitle")
 
 spacekey = sys.argv[1]
 pagetitle = sys.argv[2]
 
 page = wiki.server.confluence2.getPage(wiki.token, spacekey, pagetitle)
 if page is None:
-   exit("Could not find page " + spacekey + ":" + pagetitle)
+    exit("Could not find page " + spacekey + ":" + pagetitle)
 
-#pprint(page)
-print(page['title'])
-
-def fetchChildren(parent, gparenttitle=''):
-  children = wiki.server.confluence2.getChildren(wiki.token, parent['id'])
-  for page in children:
-    print gparenttitle + ';' + parent['title'] + ';' + page['title']
-    fetchChildren(page, parent['title'])
-
-fetchChildren(page)
+fetchChildren(page, os.getcwd())
