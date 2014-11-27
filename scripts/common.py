97abce9c0421a63ae860986c6ac33ba8318ac85a
Robbie Mackay <rm@robbiemackay.com>
2014-11-27 16:44:23 +1300

    Handle more special chars

--- scripts/common.py
+++ scripts/common.py
@@ -13,16 +13,43 @@ transform = etree.XSLT(xslt_tree)
 
 def convertToMarkdown(page):
     # Wrap Confluence XML in headers and such
-    strWrapperTop = "<?xml version=\"1.0\"?>" \
-        "<!DOCTYPE ac:confluence SYSTEM \"confluence-all.dtd\" [ " \
-        "<!ENTITY clubs    \"&#9827;\">" \
-        "<!ENTITY nbsp   \"&#160;\">" \
-        "<!ENTITY ndash   \"&#8211;\">" \
-        "<!ENTITY mdash   \"&#8212;\">" \
-        "<!ENTITY rsquo   \"&#8217;\">" \
-        "<!ENTITY lsquo   \"&#8216;\">" \
-        " ]>" \
-        "<ac:confluence xmlns:ac=\"http://www.atlassian.com/schema/confluence/4/ac/\" xmlns:ri=\"http://www.atlassian.com/schema/confluence/4/ri/\" xmlns=\"http://www.atlassian.com/schema/confluence/4/\">"
+    strWrapperTop = """<?xml version="1.0" ?>
+        <!DOCTYPE ac:confluence SYSTEM "confluence-all.dtd" [
+        <!ENTITY clubs  "&#9827;">
+        <!ENTITY nbsp   "&#160;">
+        <!-- # Grabbed a bunch from http://www.w3.org/TR/html4/sgml/entities.html#misc -->
+        <!ENTITY quot    "&#34;">
+        <!ENTITY amp     "&#38;">
+        <!ENTITY lt      "&#60;">
+        <!ENTITY gt      "&#62;">
+        <!ENTITY OElig   "&#338;">
+        <!ENTITY oelig   "&#339;">
+        <!ENTITY circ    "&#710;">
+        <!ENTITY tilde   "&#732;">
+        <!ENTITY ensp    "&#8194;">
+        <!ENTITY emsp    "&#8195;">
+        <!ENTITY thinsp  "&#8201;">
+        <!ENTITY zwnj    "&#8204;">
+        <!ENTITY zwj     "&#8205;">
+        <!ENTITY lrm     "&#8206;">
+        <!ENTITY rlm     "&#8207;">
+        <!ENTITY ndash   "&#8211;">
+        <!ENTITY mdash   "&#8212;">
+        <!ENTITY lsquo   "&#8216;">
+        <!ENTITY rsquo   "&#8217;">
+        <!ENTITY sbquo   "&#8218;">
+        <!ENTITY ldquo   "&#8220;">
+        <!ENTITY rdquo   "&#8221;">
+        <!ENTITY bdquo   "&#8222;">
+        <!ENTITY dagger  "&#8224;">
+        <!ENTITY Dagger  "&#8225;">
+        <!ENTITY permil  "&#8240;">
+        <!ENTITY lsaquo  "&#8249;">
+        <!ENTITY rsaquo  "&#8250;">
+        <!ENTITY euro   "&#8364;">
+        ]>
+        <ac:confluence xmlns:ac="http://www.atlassian.com/schema/confluence/4/ac/" xmlns:ri="http://www.atlassian.com/schema/confluence/4/ri/" xmlns="http://www.atlassian.com/schema/confluence/4/">
+        """
     strWrapperBottom = "</ac:confluence>"
     strConfluenceXMLDoc = strWrapperTop + page['content'] + strWrapperBottom
 
@@ -36,3 +63,5 @@ def convertToMarkdown(page):
         raise
 
     return "# " + page['title'] + "\n\n" + h.handle(unicode(result).encode("ascii","ignore"))
+
+
